11e582f2d0cd03231eb455d69ec536e535c0ea29
yuxiaoyuan0406 <973971990@qq.com>
2021-11-03 14:44:24 +0800

    using urllib3

--- rz.py
+++ rz.py
@@ -1,12 +1,10 @@
-import requests
+# import httplib
+import urllib3
 import json
 
-url = "http://192.168.2.231/srun_portal_pc.php?ac_id=1&"
+url = "http://192.168.2.231/"
 with open('user.json', 'r') as f:
     data = json.load(f)
 
 if __name__ == "__main__":
-    # print(data)
-    r = requests.post(url=url, data=data)
-    print(r)
-    requests.Response.ok(r)
+    print(data)
