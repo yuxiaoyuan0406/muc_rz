2f75259bcb595d6185bd8f5341156f14cd8fd7e6
yuxiaoyuan0406 <yuxiaoyuan0406@qq.com>
2021-11-05 12:55:55 +0800

    finished using urllib3

--- rz.py
+++ rz.py
@@ -1,10 +1,35 @@
+#!/usr/bin/python3
 # import httplib
 import urllib3
 import json
+from urllib.parse import urlencode
+import os
+import time
 
-url = "http://192.168.2.231/"
-with open('user.json', 'r') as f:
-    data = json.load(f)
+url = "http://192.168.2.231/srun_portal_pc.php?ac_id=1&"
+
+with open('user.json', 'r') as _:
+    post_data = json.load(_)
+    post_body = urlencode(post_data)
+_.close()
+
+with open('header.json', 'r') as _:
+    header = json.load(_)
+_.close()
+
+def post(manager, url, headers, body):
+    return manager.request(
+        method='POST',
+        url=url,
+        headers=headers,
+        body=body
+    )
 
 if __name__ == "__main__":
-    print(data)
+    manager = urllib3.PoolManager()
+    while (True):
+        if(os.system('ping www.baidu.com -c 3')):
+            print('re-connecting...')
+            r = post(manager, url, header, post_body)
+            print('response: {}'.format(r.status))
+        time.sleep(2)
